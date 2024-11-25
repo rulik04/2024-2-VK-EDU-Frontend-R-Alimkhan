a715fc0ddd9942fc2a35f6330cf6cc7437a87094
rulik04 <rulik_2004@mail.ru>
2024-11-25 16:01:55 +0500

    update vite.config.js

--- react-chat/vite.config.js
+++ react-chat/vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: "/2024-2-VK-EDU-Frontend-R-Alimkhan/",
+    base: "2024-2-VK-EDU-Frontend-R-Alimkhan",
 });
