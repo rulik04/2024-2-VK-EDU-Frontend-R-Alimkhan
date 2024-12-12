ade0189158f6cae87c660d1388c283168826eeb1
rulik04 <rulik_2004@mail.ru>
2024-12-12 22:36:52 +0500

    api console

--- react-chat/src/services/api.js
+++ react-chat/src/services/api.js
@@ -1,5 +1,6 @@
 const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 console.log("BASE_URL", BASE_URL);
+console.log("import.meta.env", import.meta.env);
 
 export const api = async (endpoint, options = {}) => {
     const accessToken = localStorage.getItem("accessToken");
